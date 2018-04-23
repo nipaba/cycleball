14853c22128b1f3defa899de2850fcb7a2428b57
Tomáš Nesvadba <nesvadbatomas@seznam.cz>
2018-04-23 22:10:15 +0200

    basics

---
+++ src/main/java/com/hellokoding/springboot/HelloController.java
@@ -0,0 +1,22 @@
+package com.hellokoding.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "TOMAS   asdasdSA");
+        
+        return "hello";
+    }
+}
--- src/main/java/com/hellokoding/springboot/WebApplication.java
+++ src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -1,14 +1,14 @@
-package com.mkyong;
+package com.hellokoding.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootWebApplication {
-
+public class WebApplication {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(SpringBootWebApplication.class, args);
+        
+        SpringApplication.run(WebApplication.class, args);
         Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler http://localhost:8080/");
     }
-
 }
+
